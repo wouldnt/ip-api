e950f64b320861ed3e9d1f1b672a0c8a9e937823
W <wouldnt@koreatown.nl>
2021-11-10 01:38:38 +0100

    Forgot to clean the validation unit test

--- src/test/validation.test.js
+++ src/test/validation.test.js
@@ -1,39 +1,38 @@
-/* eslint-disable global-require */
-const httpMock = require('node-mocks-http')
+const httpMock = require("node-mocks-http");
 const {
-  convertDate, validateData, resultValidation, countValidation
-} = require('../utils')
+  convertDate,
+  validateData,
+  resultValidation,
+  countValidation,
+} = require("../utils");
 
-let req
-let res
+let req;
+let res;
 
-/* eslint-disable no-undef */
-describe('utils custom testing ', () => {
+describe("utils custom testing ", () => {
   beforeEach(() => {
-    // create express request and response mock
-    req = httpMock.createRequest()
-    res = httpMock.createResponse()
-    next = jest.fn()
-  })
+    req = httpMock.createRequest();
+    res = httpMock.createResponse();
+    next = jest.fn();
+  });
 
-  it('convertdate', () => {
-    const getDate = convertDate({})
-    expect(convertDate({})).toHaveProperty('createdAt', getDate.createdAt)
-    expect(convertDate({})).toHaveProperty('updatedAt', getDate.updatedAt)
-  })
+  it("convertdate", () => {
+    const getDate = convertDate({});
+    expect(convertDate({})).toHaveProperty("createdAt", getDate.createdAt);
+    expect(convertDate({})).toHaveProperty("updatedAt", getDate.updatedAt);
+  });
 
-  it('validate data', () => {
-    // eslint-disable-next-line global-require
-    const msg = require('../utils/exceptions')
-    validateData(req, res, msg, 'world', { data: 'a' })
-  })
+  it("validate data", () => {
+    const msg = require("../utils/exceptions");
+    validateData(req, res, msg, "world", { data: "a" });
+  });
 
-  it('validate resultt', () => {
-    resultValidation(req)
-  })
+  it("validate resultt", () => {
+    resultValidation(req);
+  });
 
-  it('coun result', () => {
-    const msg = require('../utils/exceptions')
-    countValidation(req, res, [], msg)
-  })
-})
+  it("coun result", () => {
+    const msg = require("../utils/exceptions");
+    countValidation(req, res, [], msg);
+  });
+});
