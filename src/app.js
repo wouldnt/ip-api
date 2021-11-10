a3b87c928546d99c142224ec33fb7f2fc1d4c7d8
W <wouldnt@koreatown.nl>
2021-11-10 02:16:13 +0100

    Removed sentry

--- src/app.js
+++ src/app.js
@@ -7,8 +7,6 @@ const methodOverride = require("method-override");
 const helmet = require("helmet");
 const xss = require("xss-clean");
 const morgan = require("morgan");
-const Sentry = require("@sentry/node");
-const Tracing = require("@sentry/tracing");
 const { notFoundHandler, errorHandler } = require("./utils/exceptions");
 const { MORGAN_FORMAT } = require("./utils/constant");
 const routing = require("./routes");
@@ -23,23 +21,6 @@ app.use(express.json({ limit: "200kb" }));
 
 const morganFormat = MORGAN_FORMAT;
 app.use(morgan(morganFormat, { stream: process.stderr }));
-Sentry.init({
-  dsn: process.env.SENTRY_DSN,
-  integrations: [
-    new Sentry.Integrations.Http({ tracing: true }),
-    new Tracing.Integrations.Express({
-      app,
-    }),
-  ],
-
-  tracesSampleRate: Number(process.env.SENTRY_TRACE_RATE),
-});
-
-if (process.env.NODE_ENV !== "test") {
-  app.use(Sentry.Handlers.requestHandler());
-}
-
-app.use(Sentry.Handlers.tracingHandler());
 
 app.get("/favicon.ico", (_req, res) => {
   res.status(204);
@@ -49,7 +30,5 @@ app.use(xss());
 app.use(routing);
 app.use(notFoundHandler);
 app.use(errorHandler);
-
-app.use(Sentry.Handlers.errorHandler());
 
 module.exports = app;
